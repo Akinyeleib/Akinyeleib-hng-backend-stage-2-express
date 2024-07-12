6f5b72a65d1b27e721c985ae8a88c20ff2a31566
Ibrahim Akinyele <iakinyele3@gmail.com>
2024-07-12 06:43:38 +0100

    place port and app in different files

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const authRouter = require('./routes/auth')
+const userRouter = require('./routes/user')
+const organisationRouter = require('./routes/organisation')
+require('dotenv').config()
+
+const app = express()
+
+const PORT = process.env.PORT || 81
+
+app.use(express.json())
+
+app.use('/auth', authRouter)
+app.use('/api/users', userRouter)
+app.use('/api/organisations', organisationRouter)
+
+app.get('/', (req, res) => {
+    res.send('Hello....nndh.')
+})
+
+module.exports = app
